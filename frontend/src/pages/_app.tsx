7959620a501732590aac564e6820117e477434c3
andresromero22 <andresfromero22@gmail.com>
2025-06-13 10:37:16 -0500

    Add tRPC client setup

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,20 +2,39 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { httpBatchLink } from '@trpc/client'
+import superjson from 'superjson'
 import kaltireLogo from '../images/kaltire_logo.png'
+import { trpc } from '../utils/trpc'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      transformer: superjson,
+      links: [
+        httpBatchLink({ url: '/api/trpc' }),
+      ],
+    })
+  )
+
   return (
-    <div className="app-container">
-      <header className="header">
-        <Image src={kaltireLogo} alt="Kaltire" className="logo" />
-        <nav>
-          <Link href="/" className="nav-link">Chatbot</Link>
-          <Link href="/config" className="nav-link">Configuration</Link>
-        </nav>
-      </header>
-      <Component {...pageProps} />
-    </div>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <div className="app-container">
+          <header className="header">
+            <Image src={kaltireLogo} alt="Kaltire" className="logo" />
+            <nav>
+              <Link href="/" className="nav-link">Chatbot</Link>
+              <Link href="/config" className="nav-link">Configuration</Link>
+            </nav>
+          </header>
+          <Component {...pageProps} />
+        </div>
+      </QueryClientProvider>
+    </trpc.Provider>
   )
 }
 
